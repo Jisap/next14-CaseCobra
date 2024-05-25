6ec6fc35db47305aafcef58e441acec8fd87f19c
Jorge Matos <jisapjisap@gmail.com>
2024-05-25 13:09:15 +0200

    fix

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -1,14 +1,15 @@
 
 import React from 'react';
 import { Suspense } from 'react'
-import ThankYou from '@/app/thank-you/Gracias';
+//import ThankYou from '@/app/thank-you/Gracias';
+import Gracias from '@/app/thank-you/Gracias';
 
 
 const Page = () => {
 
   return (
     <Suspense>
-      <ThankYou />
+      <Gracias/>
     </Suspense> 
   )    
 }
